0a510a20af345beaa21ffc2fa63761d25a432ee0
Terin Sebastin <terins@kruxsoft.com>
2021-11-14 12:32:08 +0530

    Angular Form Submission using Template driven Method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-Forms';
+  @ViewChild('f') signupform: NgForm;
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  //   console.log(form.value);
+  // }
+  onSubmit() {
+    console.log(this.signupform);
+  }
 }
